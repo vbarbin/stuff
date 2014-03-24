8aef5ecc3255d55a3cf927520e1bcc48e0f7fe17
Vincent Barbin <git@vincentbarbin.net>
2014-03-23 23:26:54 -0400

    Changed for a process using promises

--- server/process.js
+++ server/process.js
@@ -1,8 +1,27 @@
+var exec = require('child_process').exec;
+var Q = require('q');
+
 function Process(socket, tag) {
     this.socket = socket;
     this.tag = tag;
     this.intervalId = null;
 }
+
+Process.prototype.createFolder = function(folderName) {
+    var deferred = Q.defer();
+    exec('mkdir d:\\crap\\' + folderName, function(err, stdout) {
+        if (err) {
+            deferred.reject(err);
+        }
+        else {
+            setTimeout(function() {
+                deferred.resolve(stdout.toString());
+            }, 6000);
+        }
+    });
+
+    return deferred.promise;
+};
 
 Process.prototype.run = function() {
     var self = this;
@@ -11,16 +30,35 @@ Process.prototype.run = function() {
         data: 'Running'
     });
 
-    var i = 1;
-    this.intervalId = setInterval(function() {
-        self.socket.emit('progress', {
-            data: self.tag + ' ' + i++
-        })
-    }, 500);
-}
+    Q().then(function() {
+        return self.createFolder(self.tag + '1')
+    }).then(function() {
+        return self.createFolder(self.tag + '2')
+    }).then(function() {
+        return self.createFolder(self.tag + '3')
+    }).then(function() {
+        return self.createFolder(self.tag + '4')
+    }).then(function() {
+        return self.createFolder(self.tag + '5')
+    }).then(function() {
+        return self.createFolder(self.tag + '6')
+    }).then(function() {
+        return self.createFolder(self.tag + '7')
+    }).then(function() {
+        return self.createFolder(self.tag + '8')
+    }).then(function() {
+        return self.createFolder(self.tag + '9')
+    }).then(function() {
+        return self.createFolder(self.tag + '10')
+    }).then(function(data) {
+        self.socket.emit('progress', { data: 'Done.'});
+    }).catch(function(err) {
+        self.socket.emit('progress', { data: err });
+    });
+};
 
 Process.prototype.cancel = function() {
     clearInterval(this.intervalId);
-}
+};
 
 module.exports = Process;
